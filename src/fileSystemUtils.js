e208254fdce94f447bc833e1094f5e3bc88e42af
armhil <arman.hilmioglu@gmail.com>
2022-11-25 21:58:59 +0000

    Fixing dir scan

--- src/fileSystemUtils.js
+++ src/fileSystemUtils.js
@@ -27,7 +27,7 @@ module.exports = {
                 let dirsInDir = currentDirectoryContents
                     .filter(t => fs.lstatSync(path.join(currentDirectoryPath, t))
                     .isDirectory()).map(t => path.join(currentDirectoryPath, t));
-                if (dirsInDir && dirsInDir.length) q.enqueue(...dirsInDir);
+                if (dirsInDir && dirsInDir.length) dirsInDir.forEach(t => q.enqueue(d));
             }
         }
 
