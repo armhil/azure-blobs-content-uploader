2c669503eb150e9c791abe1887517f650cb77d26
armhil <arman.hilmioglu@gmail.com>
2024-11-16 01:23:26 +0000

    Fix dist

--- dist/src/azure-upload-utils.js
+++ dist/src/azure-upload-utils.js
@@ -1,10 +1,15 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
-const path = require('path');
-const fs = require('fs');
-export function uploadAll(uploadConfigs, filesToUpload, supportedContentTypes) {
-    uploadConfigs.forEach(t => {
-        const blobServiceClient = BlobServiceClient.fromConnectionString(t.connectionString);
-        const containerClient = blobServiceClient.getContainerClient(t.container);
+import path from 'path';
+import fs from 'fs';
+import { BlobServiceClient } from '@azure/storage-blob';
+import { ClientSecretCredential } from '@azure/identity';
+export function uploadAll(storageAccountList, containerName, entraAppConfiguration, filesToUpload, supportedContentTypes) {
+    storageAccountList.forEach(account => {
+        // Storage account URL
+        const blobServiceClientUrl = `https://${account}.blob.core.windows.net`;
+        const credential = new ClientSecretCredential(entraAppConfiguration.tenantId, entraAppConfiguration.clientId, entraAppConfiguration.clientSecret);
+        const blobServiceClient = new BlobServiceClient(blobServiceClientUrl, credential);
+        // Get a reference to the container
+        const containerClient = blobServiceClient.getContainerClient(containerName);
         filesToUpload.forEach(x => {
             let stream = fs.readFileSync(x.absoluteDiskPath);
             let contentType = supportedContentTypes[path.extname(x.absoluteDiskPath)];
